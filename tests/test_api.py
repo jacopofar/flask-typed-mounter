83345d92036ca1ae2c7af56086e7c1b26448ac3b
Alessandro Cifani <alessandro.cifani@gmail.com>
2017-09-25 12:47:27 +0200

    Add tests for middleware

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,19 +1,31 @@
 import json
 import unittest
 
-from checker import app
+from flask import Flask
+
+from checker import TypedMounter
 
 
 class AppTest(unittest.TestCase):
     def setUp(self):
-        self.app = app.app.test_client()
+        app = Flask(__name__)
+        tm = TypedMounter(app)
+        self.app = app.test_client()
+
+        @tm.attach_endpoint('/mul', methods=['GET', 'POST'])
+        def multiplier(val1: int, val2: int = 5):
+            return val1 * val2
 
     def test_correct_call(self):
-        response = self.app.post("/mul", data=json.dumps({'val1': 4}), content_type='application/json')
+        response = self.app.post("/mul",
+                                 data=json.dumps({'val1': 4}),
+                                 content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_wrong_call(self):
-        response = self.app.post("/mul", data=json.dumps({'val1': 'not an integer!'}), content_type='application/json')
+        response = self.app.post("/mul",
+                                 data=json.dumps({'val1': 'not an integer!'}),
+                                 content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
 
